2ce3c0ec1a67cba8efb430f85e1e7b2dc85de90d
rvdforst <rolf.v.d.forst@fau.de>
2023-12-05 16:37:23 +0100

    Create selectors.js

---
+++ src/twitter/store/selectors.js
@@ -0,0 +1,13 @@
+export const getAutoshareEnabled = (state) => state.autoshareEnabled;
+
+export const getErrorMessage = (state) => state.errorMessage;
+
+export const getOverriding = (state) => state.overriding;
+
+export const getSaving = (state) => state.saving;
+
+export const getTweetText = (state) => state.tweetText;
+
+export const getAllowTweetImage = (state) => state.allowTweetImage;
+
+export const getTweetAccounts = (state) => state.tweetAccounts || [];
