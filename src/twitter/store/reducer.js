f83f4e7f95034f04a27a9d8f3de1e9a94b318073
rvdforst <rolf.v.d.forst@fau.de>
2023-12-05 16:37:21 +0100

    Create reducer.js

---
+++ src/twitter/store/reducer.js
@@ -0,0 +1,82 @@
+const { enabled, allowTweetImage, tweetAccounts, customTweetBody } =
+    adminAutoshareTwitter;
+
+import {
+    SET_AUTOSHARE_TWITTER_ENABLED,
+    SET_ERROR_MESSAGE,
+    SET_TWEET_TEXT,
+    SET_SAVING,
+    SET_LOADED,
+    SET_OVERRIDING,
+    SET_ALLOW_TWEET_IMAGE,
+    SET_TWEET_ACCOUNTS,
+} from "./constants";
+
+export const DEFAULT_STATE = {
+    autoshareEnabled: !!enabled && "0" !== enabled,
+    errorMessage: "",
+    loaded: false,
+    overriding: customTweetBody ? true : false,
+    overrideLength: 0,
+    tweetText: customTweetBody || "",
+    allowTweetImage: !!allowTweetImage,
+    tweetAccounts: tweetAccounts || [],
+};
+
+export default function reducer(state = DEFAULT_STATE, action) {
+    switch (action.type) {
+        case SET_AUTOSHARE_TWITTER_ENABLED:
+            return {
+                ...state,
+                autoshareEnabled: action.autoshareEnabled,
+            };
+
+        case SET_ERROR_MESSAGE:
+            return {
+                ...state,
+                errorMessage: action.errorMessage,
+            };
+
+        case SET_LOADED: {
+            return {
+                ...state,
+                loaded: true,
+            };
+        }
+
+        case SET_OVERRIDING: {
+            return {
+                ...state,
+                overriding: action.overriding,
+            };
+        }
+
+        case SET_SAVING: {
+            return {
+                ...state,
+                saving: action.saving,
+            };
+        }
+
+        case SET_TWEET_TEXT: {
+            return {
+                ...state,
+                tweetText: action.tweetText,
+            };
+        }
+
+        case SET_ALLOW_TWEET_IMAGE: {
+            return {
+                ...state,
+                allowTweetImage: action.allowTweetImage,
+            };
+        }
+
+        case SET_TWEET_ACCOUNTS: {
+            return {
+                ...state,
+                tweetAccounts: action.tweetAccounts,
+            };
+        }
+    }
+}
