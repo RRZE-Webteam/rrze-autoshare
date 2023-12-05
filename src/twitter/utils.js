e2036b80426709d7394b074a67de378110a01e6a
rvdforst <rolf.v.d.forst@fau.de>
2023-12-05 16:37:28 +0100

    Create utils.js

---
+++ src/twitter/utils.js
@@ -0,0 +1,25 @@
+import { Icon } from "@wordpress/components";
+import { FailedIcon, TweetedIcon, DefaultIcon } from "./components/PluginIcon";
+
+export const getIconByStatus = (tweetStatus) => {
+    let StatusIcon = DefaultIcon;
+
+    if (tweetStatus) {
+        if (tweetStatus === "published") {
+            StatusIcon = TweetedIcon;
+        } else if (tweetStatus === "error") {
+            StatusIcon = FailedIcon;
+        } else {
+            StatusIcon = DefaultIcon;
+        }
+    }
+
+    const TweetStatusIcon = (
+        <Icon
+            className="rrze-autoshare-twitter-icon"
+            icon={StatusIcon}
+            size={48}
+        />
+    );
+    return TweetStatusIcon;
+};
