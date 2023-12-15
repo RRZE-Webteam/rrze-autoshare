1a3781bd7eef003b202b4b6d612ab8f8788ba9ce
rvdforst <rolf.v.d.forst@fau.de>
2023-12-15 12:11:02 +0100

    Create Utils.php

---
+++ includes/Services/Bluesky/Utils.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RRZE\Autoshare\Services\Bluesky;
+
+defined('ABSPATH') || exit;
+
+class Utils
+{
+    public static function getConnectionStatus()
+    {
+        if (get_option(Main::ACCESS_JWT)) {
+            return __('connected', 'rrze-autoshare');
+        }
+        return __('not connected', 'rrze-autoshare');
+    }
+
+    public static function validateUrl($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_URL);
+    }
+}
