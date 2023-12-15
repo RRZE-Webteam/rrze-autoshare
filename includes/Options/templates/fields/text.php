028c9f295153fa434e311190f0b700a034605ff9
rvdforst <rolf.v.d.forst@fau.de>
2023-12-15 16:13:39 +0100

    Update text.php

--- includes/Options/templates/fields/text.php
+++ includes/Options/templates/fields/text.php
@@ -9,7 +9,7 @@ defined('ABSPATH') || exit;
         <label for="<?php echo $option->getIdAttribute(); ?>" class="<?php echo $option->getLabelClassAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
     </th>
     <td class="rrze-wp-form rrze-wp-form-input">
-        <input name="<?php echo esc_attr($option->getNameAttribute()); ?>" id="<?php echo $option->getIdAttribute(); ?>" type="text" value="<?php echo $option->getValueAttribute(); ?>" class="<?php echo $option->getInputClassAttribute(); ?>">
+        <input name="<?php echo esc_attr($option->getNameAttribute()); ?>" id="<?php echo $option->getIdAttribute(); ?>" type="text" value="<?php echo $option->getValueAttribute(); ?>" placeholder="<?php echo $option->getPlaceholderAttribute() ?: ''; ?>" class="<?php echo $option->getInputClassAttribute() ?: 'regular-text'; ?>">
         <?php if ($description = $option->getArg('description')) { ?>
             <p class="description"><?php echo $description; ?></p>
         <?php } ?>
