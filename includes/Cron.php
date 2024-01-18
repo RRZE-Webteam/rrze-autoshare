4f428fd3520230ddbdd307d25d14d39464b357fb
rvdforst <rolf.v.d.forst@fau.de>
2024-01-18 17:56:53 +0100

    Update Cron.php

--- includes/Cron.php
+++ includes/Cron.php
@@ -13,13 +13,13 @@ use RRZE\Autoshare\Services\Bluesky\Main as Bluesky;
  */
 class Cron
 {
-    const BSKY_REFRESH_TOKEN = 'rrze_autoshare_bluesky_refresh_token';
+    const BLUESKY_REFRESH_TOKEN = 'rrze_autoshare_bluesky_refresh_token';
 
     // Initialize the class by setting up action hooks
     public static function init()
     {
-        // Add an action hook to run the 'bskyRefreshToken' method when scheduled.
-        add_action(self::BSKY_REFRESH_TOKEN, [__CLASS__, 'bskyRefreshToken']);
+        // Add an action hook to run the 'blueskyRefreshToken' method when scheduled.
+        add_action(self::BLUESKY_REFRESH_TOKEN, [__CLASS__, 'blueskyRefreshToken']);
 
         // Add an action hook to activate scheduled events during WP initialization.
         add_action('init', [__CLASS__, 'activateScheduledEvents']);
@@ -29,14 +29,14 @@ class Cron
     public static function activateScheduledEvents()
     {
         // Check if the scheduled event is not already in the queue.
-        if (!wp_next_scheduled(self::BSKY_REFRESH_TOKEN)) {
+        if (!wp_next_scheduled(self::BLUESKY_REFRESH_TOKEN)) {
             // Schedule the event to run weekly starting from the current time.
-            wp_schedule_event(time(), 'weekly', self::BSKY_REFRESH_TOKEN);
+            wp_schedule_event(time(), 'weekly', self::BLUESKY_REFRESH_TOKEN);
         }
     }
 
     // Method to be executed when the scheduled event is triggered
-    public static function bskyRefreshToken()
+    public static function blueskyRefreshToken()
     {
         Bluesky::connect();
     }
@@ -45,6 +45,6 @@ class Cron
     public static function clearSchedule()
     {
         // Clear the scheduled event hook for the specified action hook.
-        wp_clear_scheduled_hook(self::BSKY_REFRESH_TOKEN);
+        wp_clear_scheduled_hook(self::BLUESKY_REFRESH_TOKEN);
     }
 }
