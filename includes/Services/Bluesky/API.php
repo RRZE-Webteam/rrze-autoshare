0fcd82c2bd9eddd1fac51eb3c9ea42da802b4ec7
rvdforst <rolf.v.d.forst@fau.de>
2024-01-19 17:35:54 +0100

    Update API.php

--- includes/Services/Bluesky/API.php
+++ includes/Services/Bluesky/API.php
@@ -16,18 +16,6 @@ class API
 
     const DID = 'rrze_autoshare_bluesky_did';
 
-    public static function init()
-    {
-        add_action('save_post', [__CLASS__, 'saveMeta'], 11, 2);
-
-        $supportedPostTypes = settings()->getOption('bluesky_post_types');
-        foreach ($supportedPostTypes as $postType) {
-            add_action("save_post_{$postType}", [__CLASS__, 'savePost'], 11, 2);
-        }
-
-        add_action('rrze_autoshare_bluesky_publish_post', [__CLASS__, 'syndicatePost']);
-    }
-
     public static function connect()
     {
         $blueskyIdentifier = settings()->getOption('bluesky_identifier');
@@ -90,51 +78,6 @@ class API
         return (bool) get_option(self::ACCESS_JWT);
     }
 
-    public static function saveMeta($postId, $post)
-    {
-        if (wp_is_post_revision($post) || wp_is_post_autosave($post)) {
-            return;
-        }
-
-        $supportedPostTypes = settings()->getOption('bluesky_post_types');
-        if (!in_array($post->post_type, $supportedPostTypes)) {
-            return;
-        }
-
-        if (!isset($_POST['rrze_autoshare_bluesky_enabled'])) {
-            return;
-        }
-
-        $metaValue = (bool) $_POST['rrze_autoshare_bluesky_enabled'];
-
-        update_metadata('post', $postId, 'rrze_autoshare_bluesky_enabled', $metaValue);
-    }
-
-    public static function savePost($postId, $post)
-    {
-        if (wp_is_post_revision($post) || wp_is_post_autosave($post)) {
-            return;
-        }
-
-        if (post_password_required($post)) {
-            return false;
-        }
-
-        $supportedPostTypes = settings()->getOption('bluesky_post_types');
-        if (!in_array($post->post_type, $supportedPostTypes)) {
-            return;
-        }
-
-        if (!self::isConnected()) {
-            return;
-        }
-
-        $autoshare = (bool) get_metadata($post->post_type, $postId, 'rrze_autoshare_bluesky_enabled', true);
-        if ($autoshare) {
-            wp_schedule_single_event(time(), 'rrze_autoshare_bluesky_publish_post', [$postId]);
-        }
-    }
-
     public static function syndicatePost($postId)
     {
         $post = get_post($postId);
@@ -191,5 +134,13 @@ class API
                 update_metadata($post->post_type, $postId, 'rrze_autoshare_bluesky_syndicated', true);
             }
         }
+    }
+
+    public static function getConnectionStatus()
+    {
+        if (get_option(self::ACCESS_JWT)) {
+            return __('connected', 'rrze-autoshare');
+        }
+        return __('not connected', 'rrze-autoshare');
     }
 }
