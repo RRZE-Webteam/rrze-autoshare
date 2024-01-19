f657034f1ba39f2732d90667e8aa4fbe80b3f307
rvdforst <rolf.v.d.forst@fau.de>
2024-01-19 17:36:04 +0100

    Update Utils.php

--- includes/Services/Bluesky/Utils.php
+++ includes/Services/Bluesky/Utils.php
@@ -6,14 +6,6 @@ defined('ABSPATH') || exit;
 
 class Utils
 {
-    public static function getConnectionStatus()
-    {
-        if (get_option(API::ACCESS_JWT)) {
-            return __('connected', 'rrze-autoshare');
-        }
-        return __('not connected', 'rrze-autoshare');
-    }
-
     public static function validateUrl($value)
     {
         return filter_var($value, FILTER_VALIDATE_URL);
