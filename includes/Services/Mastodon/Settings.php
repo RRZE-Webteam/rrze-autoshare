a461828acb3237f6b72182f253a60632b619306a
rvdforst <rolf.v.d.forst@fau.de>
2024-01-19 17:36:22 +0100

    Update Settings.php

--- includes/Services/Mastodon/Settings.php
+++ includes/Services/Mastodon/Settings.php
@@ -20,7 +20,7 @@ class Settings
                 'description' => sprintf(
                     '%1$s %2$s',
                     __('Status:', 'rrze-autoshare'),
-                    '' // Utils::getConnectionStatus()
+                    API::getConnectionStatus()
                 )
             ]
         );
@@ -37,6 +37,12 @@ class Settings
                 ]
             ]
         ]);
+        $sectionMain->addOption('text', [
+            'name' => 'mastodon_username',
+            'label' => __('Username', 'rrze-autoshare'),
+            'description' => __('The Mastodon account username.', 'rrze-autoshare'),
+            'default' => ''
+        ]);        
         $sectionMain->addOption('checkbox-multiple', [
             'name' => 'mastodon_post_types',
             'label' => __('Content Types', 'rrze-autoshare'),
@@ -46,6 +52,13 @@ class Settings
                 'page' => __('Pages', 'rrze-autoshare')
             ],
             'default' => ['post']
+        ]);
+        $sectionMain->addOption('button-link', [
+            'name' => 'mastodon_authorize_access_url',
+            'label' => __('Access', 'rrze-autoshare'),
+            'href' => [__NAMESPACE__ . '\API', 'authoriteAccessUrl'],
+            'text' => [__NAMESPACE__ . '\API', 'authorizeAccessText'],
+            'description' => [__NAMESPACE__ . '\API', 'authorizeAccessDescription'],
         ]);
     }
 }
