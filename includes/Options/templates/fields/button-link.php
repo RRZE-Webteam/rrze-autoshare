d3719998cc6b009984027378d6c0d79ad3a6df8c
rvdforst <rolf.v.d.forst@fau.de>
2024-01-19 17:35:38 +0100

    Create button-link.php

---
+++ includes/Options/templates/fields/button-link.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RRZE\Autoshare\Options;
+
+defined('ABSPATH') || exit;
+?>
+<tr valign="top">
+    <th scope="row" class="rrze-wp-form-label">
+        <label for="<?php echo $option->getIdAttribute(); ?>" class="<?php echo $option->getLabelClassAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
+    </th>
+    <td class="rrze-wp-form rrze-wp-form-input">
+        <a href="<?php echo esc_url($option->getArg('href')); ?>" class="button button-secondary">
+            <?php echo esc_html($option->getArg('text')); ?>
+        </a>
+        <?php if ($description = $option->getArg('description')) { ?>
+            <p class="description"><?php echo $description; ?></p>
+        <?php } ?>
+        <?php if ($error = $option->hasError()) { ?>
+            <div class="rrze-autoshare-error"><?php echo $error; ?></div>
+        <?php } ?>
+    </td>
+</tr>
