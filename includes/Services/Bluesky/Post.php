cb3d7feb64a64fbc2ea908de7cc5de0b2568cd56
rvdforst <rolf.v.d.forst@fau.de>
2024-01-30 18:07:16 +0100

    Update Post.php

--- includes/Services/Bluesky/Post.php
+++ includes/Services/Bluesky/Post.php
@@ -81,4 +81,50 @@ class Post
     {
         return (bool) get_metadata($postType, $postId, 'rrze_autoshare_bluesky_published', true);
     }
+
+    public static function getContent(\WP_Post $post)
+    {
+        // $permalink = esc_url_raw(wp_get_shortlink($post->ID));
+        $permalink = esc_url_raw(get_the_permalink($post->ID));
+
+        // 292 instead of 300 because of the space between body and URL and the ellipsis.
+        $textMaxLength = 292 - strlen($permalink);
+
+        // Don't use get_the_title() because may introduce texturized characters.
+        $title = $post->post_title;
+        $excerpt = self::getExcerpt($post);
+        $text = sanitize_text_field($title) . PHP_EOL . sanitize_textarea_field($excerpt);
+        $text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, get_bloginfo('charset'));
+        $textLength = mb_strlen($text);
+        $ellipsis = ''; // Initialize as empty. Will be set if the text is too long.
+
+        while ($textMaxLength < $textLength) {
+            // Don't use `&hellip;` because may display encoded.
+            $ellipsis = ' ...';
+
+            // If there are no spaces in the text for whatever reason, 
+            // truncate regardless of where spaces fall.
+            if (false === mb_strpos($text, ' ')) {
+                $text = mb_substr($text, 0, $textMaxLength);
+                break;
+            }
+
+            // Cut off the last word in the text until the text is short enough.
+            $words = explode(' ', $text);
+            array_pop($words);
+            $text = implode(' ', $words);
+            $textLength = strlen($text);
+        }
+
+        return sprintf('%s%s %s', $text, $ellipsis, $permalink);
+    }
+
+    private static function getExcerpt($post)
+    {
+        $excerpt = $post->post_excerpt;
+        $excerpt = preg_replace('~$excerptMore$~', '', $excerpt);
+        $excerpt = wp_strip_all_tags($excerpt);
+        $excerpt = html_entity_decode($excerpt, ENT_QUOTES | ENT_HTML5, get_bloginfo('charset'));
+        return $excerpt;
+    }
 }
