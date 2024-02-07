5b00a22a290b7600ccfb02dae06973c4da9f01e1
rvdforst <rolf.v.d.forst@fau.de>
2024-02-07 16:43:28 +0100

    Update build files

--- build/blockeditor.asset.php
+++ build/blockeditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '659535e3eac7eaa36f23');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '708093a402f3a480b0a2');
