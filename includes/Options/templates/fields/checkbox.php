4ce908a0e30822dbe162ec54c68bf7e19a55a10d
rvdforst <rolf.v.d.forst@fau.de>
2024-02-16 09:44:26 +0100

    Update checkbox.php

--- includes/Options/templates/fields/checkbox.php
+++ includes/Options/templates/fields/checkbox.php
@@ -5,12 +5,12 @@ namespace RRZE\Autoshare\Options;
 defined('ABSPATH') || exit;
 ?>
 <tr valign="top">
-    <th scope="row" class="rrze-wp-form-label">
-        <label for="<?php echo $option->getIdAttribute(); ?>" class="<?php echo $option->getLabelClassAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
+    <th scope="row">
+        <label for="<?php echo $option->getIdAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
     </th>
-    <td class="rrze-wp-form rrze-wp-form-input">
+    <td>
         <label>
-            <input name="<?php echo esc_attr($option->getNameAttribute()); ?>" id="<?php echo $option->getIdAttribute(); ?>" type="checkbox" value="<?php echo $option->getValueAttribute(); ?>" <?php echo $option->isChecked() ? 'checked' : null; ?> class="<?php echo $option->getInputClassAttribute(); ?>">
+            <input name="<?php echo esc_attr($option->getNameAttribute()); ?>" id="<?php echo $option->getIdAttribute(); ?>" type="checkbox" value="1" <?php checked($option->isChecked()); ?>>
             <?php echo $option->getArg('description'); ?>
         </label>
 
