55680942bad350477a92e9577bbb06f8fb686de7
rvdforst <rolf.v.d.forst@fau.de>
2024-02-16 09:44:20 +0100

    Update button-link.php

--- includes/Options/templates/fields/button-link.php
+++ includes/Options/templates/fields/button-link.php
@@ -5,10 +5,10 @@ namespace RRZE\Autoshare\Options;
 defined('ABSPATH') || exit;
 ?>
 <tr valign="top">
-    <th scope="row" class="rrze-wp-form-label">
-        <label for="<?php echo $option->getIdAttribute(); ?>" class="<?php echo $option->getLabelClassAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
+    <th scope="row">
+        <label for="<?php echo $option->getIdAttribute(); ?>"><?php echo $option->getLabel(); ?></label>
     </th>
-    <td class="rrze-wp-form rrze-wp-form-input">
+    <td>
         <input name="<?php echo esc_attr($option->getNameAttribute()); ?>" type="hidden" value="">
         <a href="<?php echo esc_url($option->getArg('href')); ?>" class="button button-secondary">
             <?php echo esc_html($option->getArg('text')); ?>
