123229cfe3d95b5b4378983d2631c1d57664accc
rvdforst <rolf.v.d.forst@fau.de>
2024-02-19 15:19:29 +0100

    Update Settings.php

--- includes/Services/Mastodon/Settings.php
+++ includes/Services/Mastodon/Settings.php
@@ -8,7 +8,7 @@ class Settings
 {
     protected $settings;
 
-    public function __construct(\RRZE\WP\Settings\Main $settings)
+    public function __construct(\RRZE\WP\Settings\Settings $settings)
     {
         $this->settings = $settings;
 
