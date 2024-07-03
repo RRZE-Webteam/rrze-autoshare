af6d5d7adc221f2858db69abbc89a4d4b77984ce
rvdforst <rolf.v.d.forst@fau.de>
2024-07-03 14:25:12 +0200

    Update Settings.php

--- includes/Services/Mastodon/Settings.php
+++ includes/Services/Mastodon/Settings.php
@@ -4,7 +4,7 @@ namespace RRZE\Autoshare\Services\Mastodon;
 
 defined('ABSPATH') || exit;
 
-use RRZE\Autoshare\Utils;
+use function RRZE\Autoshare\settings;
 
 class Settings
 {
@@ -51,7 +51,7 @@ class Settings
             'name' => 'mastodon_post_types',
             'label' => __('Content Types', 'rrze-autoshare'),
             'description' => __('Select the type of content that Autoshare could use.', 'rrze-autoshare'),
-            'options' => Utils::getPostTypes(),
+            'options' => settings()->getPostTypes(),
             'default' => ['post']
         ]);
         $sectionMain->addOption('checkbox', [
