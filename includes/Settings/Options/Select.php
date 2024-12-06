2ceac6a37a870d47056ae10169b417b121420f05
Rolf <rolf.v.d.forst@fau.de>
2024-12-06 14:26:26 +0100

    Add Settings lib

---
+++ includes/Settings/Options/Select.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RRZE\Autoshare\Settings\Options;
+
+defined('ABSPATH') || exit;
+
+class Select extends Type
+{
+    public $template = 'select';
+}
---
+++ includes/Settings/Options/Text.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RRZE\Autoshare\Settings\Options;
+
+defined('ABSPATH') || exit;
+
+class Text extends Type
+{
+    public $template = 'text';
+}
